39a977c51243021f9cf10851b47c33d776c2f65c
Henrik H. Sørensen <hhs19942@gmail.com>
2022-09-26 21:20:46 +0200

    Remove sentry

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,10 +1,8 @@
 import {ServiceRegistry} from './config/service-registry';
-import {SentryService} from './service/sentry-service';
 import {ExpressService} from './service/express-service';
 import {MqttService} from './service/mqtt-service';
 
 const serviceRegistry = new ServiceRegistry();
-serviceRegistry.registerService(new SentryService());
 const mqttService = new MqttService();
 serviceRegistry.registerService(mqttService);
 serviceRegistry.registerService(new ExpressService(mqttService));
